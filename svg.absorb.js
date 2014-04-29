88866b0a904e6aa99d50b9a37baed1a806cb5ccb
wout <wout@impinc.co.uk>
2014-04-29 10:30:29 +0200

    Added fix for absorbing svg fragments

--- svg.absorb.js
+++ svg.absorb.js
@@ -1,19 +1,19 @@
-// svg.absorb.js 0.1.1 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
+// svg.absorb.js 0.1.2 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
 ;(function() {
 
-	SVG.Absorbee = SVG.invent({
-		// Create js wrapper
-		create: function(node) {
-			this.node = node
-			this.type = node.localName
-			this.node.instance = this
-		}
+  SVG.Absorbee = SVG.invent({
+    // Create js wrapper
+    create: function(node) {
+      this.node = node
+      this.type = node.localName
+      this.node.instance = this
+    }
 
     // Inherit from SVG.Element
   , inherit: SVG.Element
 
-		// How the element is constructed
-	, construct: {
+    // How the element is constructed
+  , construct: {
       // Add absorb method to container elements
       absorb: function(raw) {
         if (typeof raw === 'string') {
@@ -21,15 +21,18 @@
           var i
             , well = document.createElement('div')
 
-          /* properly close svg tags and add them to the DOM */
-          well.innerHTML = raw
+          /* strip away newlines and properly close tags */
+          raw = raw
             .replace(/\n/, '')
             .replace(/<(\w+)([^<]+?)\/>/g, '<$1$2></$1>')
 
+          /* ensure SVG wrapper for correct element type cating */
+          well.innerHTML = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' + raw + '</svg>'
+
           /* transplant content from well to target */
-          for (i = well.childNodes.length - 1; i >= 0; i--)
-            if (well.childNodes[i].nodeType == 1)
-              this.add(new SVG.Absorbee(well.childNodes[i]), 0)
+          for (i = well.firstChild.childNodes.length - 1; i >= 0; i--)
+            if (well.firstChild.childNodes[i].nodeType == 1)
+              this.add(new SVG.Absorbee(well.firstChild.childNodes[i]), 0)
           
           /* mark temporary div for garbage collection */
           well = null
@@ -42,6 +45,6 @@
       }
     }
 
-	})
+  })
 
 }).call(this);
